7034725e39abe90f3ea6ac462095724baaa79cc1
Michael Kees <keesnetflix.1@gmail.com>
2022-02-26 21:20:02 -0800

    testing web 3

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,21 @@
 const NavBar = () => {
   return (
-    <div className="navbar">
+    <div className="navbar align-center">
       <ul>
         <li>
-          <div className="logo"></div>
+          <div className="logo align-left"></div>
         </li>
         <li>
-          <a href="/">Home</a>
+          <a href="/">MOVIES</a>
+        </li>
+        <li>
+          <a href="/">TV</a>
+        </li>
+        <li>
+          <a href="/">LIVE</a>
+        </li>
+        <li>
+          <a href="/">JUST ADDED</a>
         </li>
       </ul>
     </div>
